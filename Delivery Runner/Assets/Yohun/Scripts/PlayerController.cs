ee6571c821b9a09ff2b414afdca3967039fc1530
Muhammadyohan <142879314+Muhammadyohan@users.noreply.github.com>
2024-02-14 10:45:13 +0700

    Add Ketchup Speed Booster

--- Delivery Runner/Assets/Yohun/Scripts/PlayerController.cs
+++ Delivery Runner/Assets/Yohun/Scripts/PlayerController.cs
@@ -1,61 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool jump = false;
-    public bool slide = false;
+    private bool isJump = false;
+    private bool isSlide = false;
 
-    public Animator anim;
+    private Animator anim;
+    private Rigidbody rbody;
+    private CapsuleCollider playerCollider;
 
-    // Start is called before the first frame update
+    private bool isSpeedBoost = false;
+    [SerializeField] private float speedBoostForSpeedBooster = 1f;
+
     void Start()
     {
         anim = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody>();
+        playerCollider = GetComponent<CapsuleCollider>();
     }
 
-    // Update is called once per frame
     void Update()
-    {
+    {   
+        //Endless Move Forward
         transform.Translate(0, 0, 0.1f);
 
+        //Speed Boost Handle
+        if (isSpeedBoost)
+        {
+            transform.Translate(0, 0, speedBoostForSpeedBooster);
+            playerCollider.enabled = false;
+            rbody.isKinematic = true;
+        }
+        else
+        {
+            playerCollider.enabled = true;
+            rbody.isKinematic = false;
+        }
+
+        //Prees W for Jump
         if (Input.GetKeyDown(KeyCode.W))
-        {
-            jump = true;
-        }
+            isJump = true;
         else
-        {
-            jump = false;
-        }
-
+            isJump = false;
+        //Prees S for Slide
         if (Input.GetKeyDown(KeyCode.S))
-        {
-            slide = true;
-        }
+            isSlide = true;
         else
-        {
-            slide = false;
-        }
+            isSlide = false;
 
-        if (jump)
+        //-------------Animation Handle-------------
+        if (isJump)
         {
-            anim.SetBool("isJump", jump);
+            anim.SetBool("isJump", isJump);
             transform.Translate(0, 0.3f, 0.1f);
         }
-        else if (!jump)
+        else if (!isJump)
         {
-            anim.SetBool("isJump", jump);
+            anim.SetBool("isJump", isJump);
         }
 
-        if (slide)
+        if (isSlide)
         {
-            anim.SetBool("isSlide", slide);
+            anim.SetBool("isSlide", isSlide);
             transform.Translate(0, 0, 0.1f);
         }
-        else if (!slide)
+        else if (!isSlide)
         {
-            anim.SetBool("isSlide", slide);
+            anim.SetBool("isSlide", isSlide);
+        }
+        //--------------------------------------------
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.tag == "Speed Booster")
+        {
+            Destroy (other.gameObject);
+            StartCoroutine(SpeedBoostController());
         }
     }
+
+    IEnumerator SpeedBoostController()
+    {
+        isSpeedBoost = true;
+        yield return new WaitForSeconds(3);
+        isSpeedBoost = false;
+    }
+
 }
