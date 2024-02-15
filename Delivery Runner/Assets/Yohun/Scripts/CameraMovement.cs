3833d7ab446b27a270826aaf7c4b367a072dffd9
Muhammadyohan <142879314+Muhammadyohan@users.noreply.github.com>
2024-02-15 16:04:46 +0700

    make camera follow player on x axis and smooth camera

--- Delivery Runner/Assets/Yohun/Scripts/CameraMovement.cs
+++ Delivery Runner/Assets/Yohun/Scripts/CameraMovement.cs
@@ -5,15 +5,34 @@ public class CameraMovement : MonoBehaviour {
 
 	public GameObject player;
 	public Vector3 offset;
+    private Vector3 velocity;
+    private Vector3 newPosForSmoothing;
+    private Transform thisTransform;
+
+    [SerializeField] private float smoothTime = 0.3f;
+
+    void Awake ()
+    {
+        thisTransform = transform;
+        velocity = new Vector3(0.5f, 0.5f, 0.5f);
+    }
 
 	void Start () 
     {
 		offset = transform.position - player.transform.position;
 	}
 	
-	void LateUpdate () 
+    void Update ()
     {
-		// transform.position = player.transform.position + offset;
-        transform.position = new Vector3(transform.position.x, player.transform.position.y + offset.y, player.transform.position.z + offset.z);
-	}
+        newPosForSmoothing.x = Mathf.SmoothDamp(thisTransform.position.x, player.transform.position.x, ref velocity.x, smoothTime) + offset.x;
+        newPosForSmoothing.y = player.transform.position.y + offset.y;
+        newPosForSmoothing.z = player.transform.position.z + offset.z;
+        transform.position = Vector3.Slerp(thisTransform.position, newPosForSmoothing, Time.time);
+    }
+
+	// void LateUpdate () 
+    // {
+	// 	// transform.position = player.transform.position + offset;
+    //     // transform.position = new Vector3(transform.position.x, player.transform.position.y + offset.y, player.transform.position.z + offset.z);
+	// }
 }
