0c1b8e7cf6185097ede2fde299b64e67c86c636a
Muhammadyohan <142879314+Muhammadyohan@users.noreply.github.com>
2024-02-18 15:14:03 +0700

    .

--- Delivery Runner/Assets/Yohun/Scripts/Obstacle/CarMovement.cs
+++ Delivery Runner/Assets/Yohun/Scripts/Obstacle/CarMovement.cs
@@ -7,13 +7,22 @@ public class CarMovement : MonoBehaviour
     [SerializeField] private float carSpeed = 1f;
     [SerializeField] private Vector3 checkRadius;
 
+    private PlayerMovement playerMovement;
+
     public LayerMask player;
     private Collider[] colliders;
+
+    void Start() 
+    {
+        
+    }
+
     void Update()
     {
         colliders = Physics.OverlapBox(transform.position, checkRadius, Quaternion.identity, player);
         foreach (Collider coll in colliders)
         {
+
             if (coll.gameObject.tag == "Player")
             {
                 transform.Translate(0, 0, carSpeed);
