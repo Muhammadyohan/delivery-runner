3ff33db9a4e35880980bf032bf1ee7c78c3a17be
Muhammadyohan <142879314+Muhammadyohan@users.noreply.github.com>
2024-02-18 13:22:41 +0700

    fix conflict 2

--- Delivery Runner/Assets/Main/Scripts/MainMenu.cs
+++ Delivery Runner/Assets/Main/Scripts/MainMenu.cs
@@ -2,24 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-<<<<<<< HEAD
-using UnityEngine.UI;
 
-=======
->>>>>>> a245512677b41ba41afd4570982510acdb6970b0
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-<<<<<<< HEAD
         SceneManager.LoadSceneAsync(1);
     }
-
-=======
-        SceneManager.LoadScene("CharacterSelectionScene");
-
-    }
->>>>>>> a245512677b41ba41afd4570982510acdb6970b0
     public void QuitGame()
     {
         Application.Quit();
